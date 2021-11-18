61d15b36a06c6f96b18d5ff2754d8efd4a652f67
Michael Velasquez <tgmike77@gmail.com>
2021-11-18 01:19:29 -0800

    Connected heroku mysql database to node server (#32)

--- back-end/app.js
+++ back-end/app.js
@@ -1,16 +1,40 @@
-const http = require('http');
+const mysql = require('mysql');
+const express = require("express");
+require('dotenv').config();
 
 const hostname = '127.0.0.1';
 const port = 3000;
+const pool = dbConnection();
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-app.get("/url", (req, res, next) => {
-  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+function dbConnection() {
+  const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.HOSTNAME,
+    user: process.env.UN,
+    password: process.env.PASSWORD,
+    database: process.env.DB_NAME
+  });
+  return pool;
+}
+
+app.get("/", (req, res, next) => {
+  res.send("Hello World!");
  });
-server.listen(port, hostname, () => {
+
+app.get("/url", (req, res, next) => {
+  res.json({"test": ["Tony","Lisa","Michael","Ginger","Food"]});
+ });
+ 
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+async function executeSQL(sql, params) {
+  return new Promise (function (resolve, reject) {
+    pool.query(sql, params, function (err, rows, fields) {
+    if (err) throw err;
+      resolve(rows);
+    });
+  });
+}
