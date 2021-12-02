7ef431b93b5604a31861b6c5edbc324f53062d68
ericchavez831 <ericchavez831@gmail.com>
2021-12-02 08:13:48 -0800

    Added Login page.

--- app/src/main/java/com/example/cst438_project03_group08/LoginPage.java
+++ app/src/main/java/com/example/cst438_project03_group08/LoginPage.java
@@ -2,17 +2,13 @@ package com.example.cst438_project03_group08;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Intent i = new Intent(MainActivity.this, LoginPage.class);
-        startActivity(i);
+        setContentView(R.layout.activity_login_page);
     }
 }
