c5bac6c64f51e16f56615c3e7629b2f687063e91
Michael Velasquez <tgmike77@gmail.com>
2021-12-01 23:21:33 -0800

    Fixed port issue with heroku

--- back-end/app.js
+++ back-end/app.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const express = require("express");
 require('dotenv').config();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const pool = dbConnection();
 const app = express();
 
