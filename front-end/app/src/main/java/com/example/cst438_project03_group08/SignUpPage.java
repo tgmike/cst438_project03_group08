3e7336b9ed5466267c8c8ab6266f3ce33ced0e99
ericchavez831 <53790574+ericchavez831@users.noreply.github.com>
2021-12-02 09:47:53 -0800

    Finished and added all pages to this branch. Create a new branch because of a file structure issues in older branches. (#37)

--- front-end/app/src/main/java/com/example/cst438_project03_group08/SignUpPage.java
+++ front-end/app/src/main/java/com/example/cst438_project03_group08/SignUpPage.java
@@ -2,16 +2,13 @@ package com.example.cst438_project03_group08;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUpPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-//        Intent i = new Intent(MainActivity.this, LoginPage.class);
-//        startActivity(i);
+        setContentView(R.layout.activity_sign_up_page);
     }
 }
