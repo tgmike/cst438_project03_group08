0f9203e0091c479feef27776b49cd94184704482
Michael Velasquez <tgmike77@gmail.com>
2021-12-07 18:27:03 -0800

    Added new routes

--- back-end/app.js
+++ back-end/app.js
@@ -1,10 +1,14 @@
 const mysql = require('mysql');
 const express = require("express");
 require('dotenv').config();
-
+const cors = require('cors');
 const port = process.env.PORT || 3000;
 const pool = dbConnection();
 const app = express();
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 function dbConnection() {
   const pool = mysql.createPool({
@@ -17,17 +21,17 @@ function dbConnection() {
   return pool;
 }
 
-app.get("/", (req, res, next) => {
+app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/books", async (req, res, next) => {
+app.get("/books", async (req, res) => {
   let booksSql = "SELECT * FROM Books";
   let books = await executeSQL(booksSql);
   res.json(books);
 });
 
-app.get("/book", async (req, res, next) => {
+app.get("/book", async (req, res) => {
   let bookId = req.query.bookId;
   let bookSql = "SELECT * FROM Books where bookId = ?";
   let param = [bookId]
@@ -38,6 +42,31 @@ app.get("/book", async (req, res, next) => {
     });
   }
   res.json(book[0]);
+});
+
+app.delete("/book", async (req, res) => {
+  let bookId = req.query.bookId;
+  let bookSql = "DELETE FROM Books where bookId = ?";
+  let param = [bookId]
+  let book = await executeSQL(bookSql, param);
+  if(book.affectedRows == 0){
+    return res.status(200).send({
+      message: 'Error book not found'
+    });
+  }else{
+    return res.status(404).send({
+      message: 'Book successfully deleted'
+    });
+  }
+});
+
+app.post("/book", async (req, res) => {
+  let title = req.body.title;
+  let author = req.body.author;
+  let bookInsertSql = "INSERT INTO Books (title, author) VALUES (?, ?)";
+  let params = [title, author];
+  let insertBook = await executeSQL(bookInsertSql, params)[0];
+  res.json(insertBook);
 });
  
 app.listen(port, () => {
