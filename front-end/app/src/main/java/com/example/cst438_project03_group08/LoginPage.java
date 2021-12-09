b524bae3eed99ea4fa3ed5f0ffd71b1822630776
ericchavez831 <ericchavez831@gmail.com>
2021-12-08 20:25:30 -0800

    Create BookAvailabilityPage but also made buttons work and redirect you to different activities.

--- front-end/app/src/main/java/com/example/cst438_project03_group08/LoginPage.java
+++ front-end/app/src/main/java/com/example/cst438_project03_group08/LoginPage.java
@@ -2,13 +2,51 @@ package com.example.cst438_project03_group08;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class LoginPage extends AppCompatActivity {
+
+    ImageButton btnBack;
+    Button btnLogin;
+    TextView tvSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        btnBack = findViewById(R.id.btnBack);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvSignUp = findViewById(R.id.tvSignUp);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginPage.this, HomePage.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginPage.this, SignUpPage.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
