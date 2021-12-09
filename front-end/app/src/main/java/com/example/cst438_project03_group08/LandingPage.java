074d81b2f2552bfc5fa8600c819d0751dc8b3626
ericchavez831 <53790574+ericchavez831@users.noreply.github.com>
2021-12-08 21:57:14 -0800

    Adding mock data for available Books (#41)
    * Create BookAvailabilityPage but also made buttons work and redirect you to different activities.
    
    * Add mock data and created data model for Books.

--- front-end/app/src/main/java/com/example/cst438_project03_group08/LandingPage.java
+++ front-end/app/src/main/java/com/example/cst438_project03_group08/LandingPage.java
@@ -1,14 +1,38 @@
 package com.example.cst438_project03_group08;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LandingPage extends AppCompatActivity {
+
+    Button btnLogin;
+    Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LandingPage.this, LoginPage.class);
+                startActivity(i);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LandingPage.this, SignUpPage.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- front-end/app/src/main/java/com/example/cst438_project03_group08/SignUpPage.java
+++ front-end/app/src/main/java/com/example/cst438_project03_group08/SignUpPage.java
@@ -3,12 +3,25 @@ package com.example.cst438_project03_group08;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class SignUpPage extends AppCompatActivity {
+
+    ImageButton btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
+
+        btnBack = findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
