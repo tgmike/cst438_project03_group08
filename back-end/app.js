2446e449f8def32d9bf0e55095f27c60f741d834
ivan776 <43163898+ivan776@users.noreply.github.com>
2021-12-09 02:01:40 -0800

    Current reservations (#49)
    * Added route to show all current reservations for admin
    
    * Added routes to create users and to look for a users current reservations
    
    * Added delete reservation route to delete any current reservation
    
    * Added route to check all available books and to delete reservations

--- back-end/app.js
+++ back-end/app.js
@@ -84,12 +84,54 @@ app.get("/reservations", async (req, res) => {
 })
 //Returns user's current reservation
 app.get("/reservation", async (req, res) => {
-let user = req.query.userId;
-let reservationsSql = "SELECT * FROM Reservations WHERE userId = ?";
-let param = [user];
-let reservations = await executeSQL(reservationsSql, param);
-res.json(reservations);
+  let user = req.query.userId;
+  let reservationsSql = "SELECT * FROM Reservations WHERE userId = ?";
+  let param = [user];
+  let reservations = await executeSQL(reservationsSql, param);
+  res.json(reservations);
 });
+
+app.delete("/deleteRes", async (req, res) => {
+  let reservationId = req.query.reservationId;
+  let reservationSql = "DELETE FROM Reservations WHERE reservationId = ?";
+  let param = [reservationId];
+  let reservation = await executeSQL(reservationSql, param);
+  if(reservation.affectedRows == 0){
+    return res.status(200).send({
+      message: 'Reservation not found'
+    });
+  }
+  else{
+    return res.status(200).send({
+      message: 'Reservation deleted'
+    });
+  }
+});
+
+app.get("/availability", async (req, res) => {
+let booksAv = [];
+let booksSql = "SELECT * FROM Books";
+let reservationsSql = "SELECT * FROM Reservations";
+let reservations = await executeSQL(reservationsSql);
+let books = await executeSQL(booksSql);
+let count = 0;
+for(let i = 0; i < books.length; i++){
+  for(let x = 0; x < reservations.length; x++){
+    if(books[i].bookId==reservations[x].bookId){
+      break;
+    }
+    else{
+      count+=1;
+    }
+  }
+  if(count!=0){
+    booksAv.push(books[i]);
+    count = 0;
+  }
+}
+res.json(booksAv);
+});
+
 app.listen(port, () => {
   console.log(`Server running at ${port}`);
 });
