a3806a767e05311a61a2517ed1219006af923d47
csramirez <oscramirez@csumb.edu>
2021-12-09 01:24:38 -0800

    Login handling. Added login dialogue box that reroutes to home page

---
+++ front-end/app/src/main/java/com/example/cst438_project03_group08/LoginResult.java
@@ -0,0 +1,18 @@
+package com.example.cst438_project03_group08;
+
+import com.google.gson.annotations.SerializedName;
+
+public class LoginResult {
+
+    private String name;
+
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
