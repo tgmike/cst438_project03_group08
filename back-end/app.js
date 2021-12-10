d531445f3c8414ea4ed1c08a7ecef14659cb0055
Michael Velasquez <tgmike77@gmail.com>
2021-12-10 14:18:19 -0800

    Fixed availability route (#53)
    * Added node skeleton
    
    * Added package.json
    
    * Added all books route and specific book route to API backend
    
    * moving frontend files to new folder for heroku deployment
    
    * Testing
    
    * Fixed port issue with heroku
    
    * Backend now runs on heroku. Frontend should be working properly.
    
    * Added new routes
    
    * Finished most user routes
    
    * Added create reservation route
    
    * Heroku things
    
    * more heroku
    
    * roku
    
    * Fixed specific reservation route
    
    * Added new route that returns all reservations for a given user
    
    * Fixed(?) availibility route

--- back-end/app.js
+++ back-end/app.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
 function dbConnection() {
   const pool = mysql.createPool({
     connectionLimit: 10,
@@ -59,15 +60,7 @@ app.delete("/book", async (req, res) => {
     });
   }
 });
-//creates users
-app.post("/createAccount", async (req, res) => {
-  let username = req.body.username;
-  let password = req.body.password;
-  let createSql = "INSERT INTO Users (username, password) VALUES (?, ?)";
-  let params = [username, password];
-  let insertUser = await executeSQL(createSql, params);
-  res.json(insertUser);
-});
+
 app.post("/book", async (req, res) => {
   let title = req.body.title;
   let author = req.body.author;
@@ -113,62 +106,52 @@ app.delete("/user", async (req, res) => {
   }
 });
 
-app.post("/reservation", async (req, res) => {
-  let date1 = new Date();
-  const format1 = "YYYY-MM-DD HH:mm:ss";
-  let reservationDateTime = moment(date1).format(format1);
-  let userId = req.body.userId;
-  let bookId = req.body.bookId;
-  //Check if user exists
-  let userSql = "SELECT * FROM Users where userId = ?";
-  let param = [userId]
-  let user = await executeSQL(userSql, param);
-  if(user.length == 0){
-    return res.status(404).send({
-      message: 'Error user not found'
-    });
-  }
-  //Check if book exists
-  let bookSql = "SELECT * FROM Books where bookId = ?";
-  param = [bookId]
-  let book = await executeSQL(bookSql, param);
-  if(book.length == 0){
-    return res.status(404).send({
-      message: 'Error book not found'
-    });
-  }
-  //Check to see if book is reserved
-  let reservationSql = "SELECT * FROM Reservations where bookId = ?";
-  param = [bookId]
-  book = await executeSQL(reservationSql, param);
-  if(book.length != 0){
-    return res.status(404).send({
-      message: 'Error book is already reserved'
-    });
-  }
-
-  let reservationInsertSql = "INSERT INTO Reservations (reservationDateTime, userId, bookId) VALUES (?, ?, ?)";
-  let params = [reservationDateTime, userId, bookId];
-  let insertReservation = await executeSQL(reservationInsertSql, params)[0];
-  res.json(insertReservation);
+//creates users
+app.post("/createAccount", async (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  let createSql = "INSERT INTO Users (username, password) VALUES (?, ?)";
+  let params = [username, password];
+  let insertUser = await executeSQL(createSql, params);
+  res.json(insertUser);
 });
  
 //gets all reservations from all users
-app.get("/reservations", async (req, res) => {
+app.get("/reservationsAll", async (req, res) => {
   let reservationsSql = "SELECT * FROM Reservations";
   let reservations = await executeSQL(reservationsSql);
   res.json(reservations);
-})
-//Returns user's current reservation
+});
+
+//Returns all of a specified user's reservations
+app.get("/reservationsUser", async (req, res) => {
+  let userId = req.query.userId;
+  let reservationsSql = "SELECT * FROM Reservations WHERE userId = ?";
+  let param = [userId];
+  let reservations = await executeSQL(reservationsSql, param);
+  if(reservations.length == 0){
+    return res.status(404).send({
+      message: 'User doesn\'t exist or has no reservations'
+    });
+  }
+  res.json(reservations);
+});
+
+//Returns a specified reservation
 app.get("/reservation", async (req, res) => {
   let reservation = req.query.reservationId;
   let reservationsSql = "SELECT * FROM Reservations WHERE reservationId = ?";
   let param = [reservation];
   let reservations = await executeSQL(reservationsSql, param);
+  if(reservations.length == 0){
+    return res.status(404).send({
+      message: 'Error reservation not found'
+    });
+  }
   res.json(reservations);
 });
 
-app.delete("/deleteRes", async (req, res) => {
+app.delete("/reservation", async (req, res) => {
   let reservationId = req.query.reservationId;
   let reservationSql = "DELETE FROM Reservations WHERE reservationId = ?";
   let param = [reservationId];
@@ -185,28 +168,69 @@ app.delete("/deleteRes", async (req, res) => {
   }
 });
 
+app.post("/reservation", async (req, res) => {
+  let date1 = new Date();
+  const format1 = "YYYY-MM-DD HH:mm:ss";
+  let reservationDateTime = moment(date1).format(format1);
+  let userId = req.body.userId;
+  let bookId = req.body.bookId;
+
+  //Check if user exists
+  let userSql = "SELECT * FROM Users where userId = ?";
+  let param = [userId]
+  let user = await executeSQL(userSql, param);
+  if(user.length == 0){
+    return res.status(404).send({
+      message: 'Error user not found'
+    });
+  }
+
+  //Check if book exists
+  let bookSql = "SELECT * FROM Books where bookId = ?";
+  param = [bookId]
+  let book = await executeSQL(bookSql, param);
+  if(book.length == 0){
+    return res.status(404).send({
+      message: 'Error book not found'
+    });
+  }
+
+  //Check to see if book is reserved
+  let reservationSql = "SELECT * FROM Reservations where bookId = ?";
+  param = [bookId]
+  book = await executeSQL(reservationSql, param);
+  if(book.length != 0){
+    return res.status(404).send({
+      message: 'Error book is already reserved'
+    });
+  }
+
+  let reservationInsertSql = "INSERT INTO Reservations (reservationDateTime, userId, bookId) VALUES (?, ?, ?)";
+  let params = [reservationDateTime, userId, bookId];
+  let insertReservation = await executeSQL(reservationInsertSql, params)[0];
+  res.json(insertReservation);
+});
+
 app.get("/availability", async (req, res) => {
-let booksAv = [];
-let booksSql = "SELECT * FROM Books";
-let reservationsSql = "SELECT * FROM Reservations";
-let reservations = await executeSQL(reservationsSql);
-let books = await executeSQL(booksSql);
-let count = 0;
-for(let i = 0; i < books.length; i++){
-  for(let x = 0; x < reservations.length; x++){
-    if(books[i].bookId==reservations[x].bookId){
-      break;
-    }
-    else{
-      count+=1;
+  let booksAv = [];
+  let bookId;
+  let param;
+  let book;
+  let reservationsSql;
+  let bookReserved;
+  let booksSql = "SELECT * FROM Books";
+  let books = await executeSQL(booksSql);
+  for(const index in books) {
+    book = books[index];
+    bookId = book.bookId;
+    reservationsSql = "SELECT * FROM Reservations WHERE bookId = ?";
+    param = [bookId];
+    bookReserved = await executeSQL(reservationsSql, param);
+    if(bookReserved.length == 0){
+      booksAv.push(book);
     }
   }
-  if(count!=0){
-    booksAv.push(books[i]);
-    count = 0;
-  }
-}
-res.json(booksAv);
+  res.json(booksAv);
 });
 
 
