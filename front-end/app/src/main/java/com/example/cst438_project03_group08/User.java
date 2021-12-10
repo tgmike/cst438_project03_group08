8767aef407afbda1f2cc4569b072f0d478582d8e
csramirez <oscramirez@csumb.edu>
2021-12-10 14:12:43 -0800

    adding missing files

---
+++ front-end/app/src/main/java/com/example/cst438_project03_group08/User.java
@@ -0,0 +1,20 @@
+package com.example.cst438_project03_group08;
+
+public class User {
+    private int userId;
+    private String username;
+    private String password;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
