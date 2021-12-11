d256a4a70ef735da1b9cb1734bfa8d540f2f0b7a
ericchavez831 <53790574+ericchavez831@users.noreply.github.com>
2021-12-10 17:54:35 -0800

    Adding all view reservation activities (#56)
    * Create BookAvailabilityPage but also made buttons work and redirect you to different activities.
    
    * Add mock data and created data model for Books.
    
    * testing getBooks endpoint
    
    * getAvailable books working in activity using retrofit and recycler view
    
    * Adding reserveBook button functionality. Fixing connection to backend.
    
    * Book availability and login now working together.
    
    * Book reservation through app should now work
    
    * add deleting reservation functionality to the applications.
    
    * Added view MyReservations and view AllReservations so that users can view all the different reservations.
    
    Co-authored-by: Michael Velasquez <tgmike77@gmail.com>

--- front-end/app/src/main/java/com/example/cst438_project03_group08/HomePage.java
+++ front-end/app/src/main/java/com/example/cst438_project03_group08/HomePage.java
@@ -10,19 +10,50 @@ import android.widget.Button;
 public class HomePage extends AppCompatActivity {
 
     Button btnAvailable;
+    Button btnMyReservations;
+    Button btnAllReservations;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
         btnAvailable = findViewById(R.id.btnAvailable);
+        btnMyReservations = findViewById(R.id.btnReservations);
+        btnAllReservations = findViewById(R.id.btnAllReservation);
 
         btnAvailable.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(HomePage.this, BookAvailabilityPage.class);
+                String user_id;
+                user_id = getIntent().getStringExtra("UserId");
+                i.putExtra("UserId", user_id);
                 startActivity(i);
             }
         });
+
+        btnMyReservations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(HomePage.this, MyReservations.class);
+                String user_id;
+                user_id = getIntent().getStringExtra("UserId");
+                i.putExtra("UserId", user_id);
+                startActivity(i);
+            }
+        });
+
+        btnAllReservations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(HomePage.this, AllReservations.class);
+                String user_id;
+                user_id = getIntent().getStringExtra("UserId");
+                i.putExtra("UserId", user_id);
+                startActivity(i);
+            }
+        });
+
     }
 }
--- front-end/app/src/main/java/com/example/cst438_project03_group08/MyReservations.java
+++ front-end/app/src/main/java/com/example/cst438_project03_group08/MyReservations.java
@@ -4,54 +4,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class BookAvailabilityPage extends AppCompatActivity {
+public class MyReservations extends AppCompatActivity {
+
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
-    private String book_id, user_id;
+    private String book_id, user_id, reservation_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book_availability_page);
+        setContentView(R.layout.activity_my_reservations);
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://cst438-project03-group08.herokuapp.com")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        List<Book> books;
-        books = new ArrayList<>();
+        List<Reservations> reservations;
+        reservations = new ArrayList<>();
         mLayoutManager = new LinearLayoutManager(this);
         bookAPI bookService = retrofit.create(bookAPI.class);
-        Call<List<Book>> call = bookService.getBooks();
         user_id = getIntent().getStringExtra("UserId");
-        book_id = getIntent().getStringExtra("BookId");
+        reservation_id = getIntent().getStringExtra("ReservationId");
 
-        // if bookID not empty, then (reserve the book using route)
-        if(book_id != null){
-            Call<Void> reserveCall = bookService.reserveBook(Integer.valueOf(user_id), Integer.valueOf(book_id));
-            reserveCall.enqueue(new Callback<Void>() {
+        // found a reservation to delete
+        if(reservation_id != null){
+            Call<Void> deleteCall = bookService.deleteMyReservation(Integer.valueOf(reservation_id));
+            deleteCall.enqueue(new Callback<Void>() {
                 @Override
                 public void onResponse(Call<Void> call, Response<Void> response) {
-                    // Log.d("RESPONSE", response.toString());
-                    Toast.makeText(BookAvailabilityPage.this, "RESERVED", Toast.LENGTH_SHORT).show();
-                    Intent i = new Intent(BookAvailabilityPage.this, HomePage.class);
+                    Toast.makeText(MyReservations.this, "DELETED", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(MyReservations.this, HomePage.class);
                     i.putExtra("UserId", user_id);
                     startActivity(i);
                 }
@@ -59,37 +57,42 @@ public class BookAvailabilityPage extends AppCompatActivity {
                 @Override
                 public void onFailure(Call<Void> call, Throwable t) {
                     // Log.d("FAILURE", call.toString());
-                    Toast.makeText(BookAvailabilityPage.this, "FAILED TO RESERVE", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MyReservations.this, "FAILED TO DELETE", Toast.LENGTH_SHORT).show();
                 }
             });
+
         }
 
-        call.enqueue(new Callback<List<Book>>() {
+
+        Call<List<Reservations>> call = bookService.getMyReservations(Integer.valueOf(user_id));
+        call.enqueue(new Callback<List<Reservations>>() {
             @Override
-            public void onResponse(Call<List<Book>> call, Response<List<Book>> response) {
+            public void onResponse(Call<List<Reservations>> call, Response<List<Reservations>> response) {
                 if (response.isSuccessful()) {
-                    for(Book book: response.body()){
-                        // Log.d("BOOK", book.toString());
-                        books.add(book);
+                    for(Reservations reservation: response.body()){
+                        // Log.d("RESERVATION", reservation.toString());
+                        reservations.add(reservation);
                     }
                 }
 
-                // Adding the books to recycler view
-                mRecyclerView = findViewById(R.id.rvBooks);
+                // Adding the reservations to recycler view
+                mRecyclerView = findViewById(R.id.rvMyReservations);
                 mRecyclerView.setHasFixedSize(true);
                 // Log.d("USERID", user_id);
                 Integer u_id;
                 u_id = Integer.valueOf(user_id);
-                mAdapter = new BookAdapter(books, BookAvailabilityPage.this, u_id);
+                mAdapter = new ReservationsAdapter(reservations, MyReservations.this, u_id);
                 mRecyclerView.setLayoutManager(mLayoutManager);
                 mRecyclerView.setAdapter(mAdapter);
             }
 
             @Override
-            public void onFailure(Call<List<Book>> call, Throwable t) {
+            public void onFailure(Call<List<Reservations>> call, Throwable t) {
                 // Log.d("BOOK", "Running error");
-                Toast.makeText(BookAvailabilityPage.this, "Fail to get the data..", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MyReservations.this, "Fail to get the data..", Toast.LENGTH_SHORT).show();
             }
         });
+
+
     }
 }
